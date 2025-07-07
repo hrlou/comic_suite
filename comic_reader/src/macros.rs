0c654751829a8f4d85071f3ff144ae4cf7378e2f
Hess Lewis <hrl@hesslewis.com>
2025-07-07 12:11:51 -0400

    Macros simplify

--- comic_reader/src/macros.rs
+++ comic_reader/src/macros.rs
@@ -1,17 +1,28 @@
 #[macro_export]
+macro_rules! comic_exts {
+    () => {
+        {
+            let mut v = vec!["cbz", "zip"];
+            #[cfg(feature = "rar")]
+            {
+                v.push("cbr");
+                v.push("rar");
+            }
+            v
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! comic_filters {
     () => {{
         let dlg = rfd::FileDialog::new();
-
+        let exts = $crate::comic_exts!();
+        let dlg = dlg.add_filter("Comic Book Archive", &exts);
         #[cfg(feature = "rar")]
         let dlg = dlg
-            .add_filter("Comic Book Archive", &["cbz", "zip", "cbr", "rar"])
             .add_filter("Comic ZIP", &["cbz", "zip"])
             .add_filter("Comic RAR", &["cbr", "rar"]);
-
-        #[cfg(not(feature = "rar"))]
-        let dlg = dlg.add_filter("Comic Book Archive", &["cbz", "zip"]);
-
         dlg
     }};
 }
