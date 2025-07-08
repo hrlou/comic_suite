2a78d5f7cd626806228b0cdedce636e6ba62eb87
Hess Lewis <hrl@hesslewis.com>
2025-07-07 23:19:09 -0400

    whoops

--- comic_reader/build.rs
+++ comic_reader/build.rs
@@ -21,8 +21,8 @@ SolidCompression=yes
 [Files]
 Source: "release\comic_reader.exe"; DestDir: "{{app}}"; Flags: ignoreversion
 Source: "release\comic_thumbgen.exe"; DestDir: "{{app}}"; Flags: ignoreversion
-Source: "..\README.md; DestDir: "{{app}}"; Flags: ignoreversion
-Source: "..\LICENSE.md; DestDir: "{{app}}"; Flags: ignoreversion
+Source: "..\README.md"; DestDir: "{{app}}"; Flags: ignoreversion
+Source: "..\LICENSE.md"; DestDir: "{{app}}"; Flags: ignoreversion
 Source: "..\comic_reader\assets\*"; DestDir: "{{app}}\assets"; Flags: ignoreversion recursesubdirs createallsubdirs
 
 [Icons]
